272cd198532e97b6b7feae952ab072355191ff68
Shrishail <shrishailchincholi306@gmail.com>
2025-11-08 12:11:52 +0530

    day1 code Changed

--- day2.c
+++ day2.c
@@ -45,3 +45,58 @@
 // 4
 // Vishal  
 
+// Example WCP to give Grades to a Student
+// #include<stdio.h>
+// int main(){
+//     int Mark;
+//     printf("Enter Your Marks 30 to 100 \n");
+//     scanf("%d",&Mark);
+//     if(Mark<=30){
+//         printf("Your Grade is C \n");
+//         printf("  Fail.... \n");
+//     } else if (Mark>30 && Mark<=70)
+//     {
+//         printf("Your Grade is B \n");
+//         printf(" Not Bad Give More your best \n");
+//     }else if (Mark>70 && Mark<=90)
+//     {
+//        printf("Your Grade is A \n");
+//        printf("Very Good:-)");
+//     }else if (Mark>90 && Mark<=100)
+//     {
+//         printf("Your Grade is A+ \n");
+//         printf("Excellent well Done ");
+//     }
+    
+//     return 0;
+// }
+
+// OutPut
+// Enter Your Marks 30 to 100 
+// 30
+// Your Grade is C 
+//   Fail.... 
+
+// WPP the Enter Charcter is upper case or lower case Letter
+// #include<stdio.h>
+// int main() {
+//     char ch;
+//     printf("Enter Letter :- \n ");
+//     scanf("%c",&ch);
+//     if(ch >= 'A' && ch<='Z'){
+//         printf("%c is Upper Case Letter",ch);
+//     }else if (ch >= 'a' && ch <= 'z' )
+//     {
+//          printf("%c is Lower Case Letter",ch);
+//     } else{
+//         printf("Not A English Letter");
+//     }
+    
+//     return 0;
+// }
+
+// OutPut 
+// Enter Letter :- 
+//  M
+// M is Upper Case Letter
+
