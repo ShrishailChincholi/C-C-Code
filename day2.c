6f0a3108e87dcd21445fa08a3bb5b418e4766bfd
Shrishail <shrishailchincholi306@gmail.com>
2025-11-08 11:13:21 +0530

    day2 code

---
+++ day2.c
@@ -0,0 +1,47 @@
+// Day 2 of Learning C programm
+// Ternary Exmaple
+
+// #include<stdio.h>               
+// int main(){
+//     int age ;
+//     printf("Enter your age : \n");
+//     scanf("%d",&age);
+//     age>18?printf("Adult\n:"):printf("Not Adult\n");//Ternary Opreator
+//     return 0;
+// };
+// OutPut 
+// Enter your age : 
+// 16
+// Not Adult
+
+
+// Switch Case Example
+// #include<stdio.h>
+// int main(){
+//     int Id;
+//     printf("Enter your Id 1-5:\n");
+//     scanf("%d",&Id);
+//     switch (Id)
+//     {
+//         case 1 : printf("Shrishail  ");
+//         break;
+//         case 2 : printf("Raju  ");
+//         break;
+//         case 3 : printf("Shanshank  ");
+//         break;
+//         case 4 : printf("Vishal  ");
+//         break;
+//         case 5 : printf("Vinod  ");
+//         break;
+
+//         default : printf("Invalid Id Plzz try agian.........");
+//     }
+
+//     return 0;
+// }
+
+// Output
+// Enter your Id:
+// 4
+// Vishal  
+
