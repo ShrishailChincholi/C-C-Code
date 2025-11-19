a9dea9f82c7fef1e54a7d720aebd6e4235363ce8
Shrishail <shrishailchincholi306@gmail.com>
2025-11-19 21:37:52 +0530

    Searching Techinque Small exmple

--- exmples.c
+++ exmples.c
@@ -1,38 +1,38 @@
 // // // Allocate memory to store 1st 5 odd numbers, then reallocate it to store first 6 even number.
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main()
-// {
-//     int *ptr;
-//     ptr = (int *)calloc(5, sizeof(int));
-//     ptr[0] = 1;
-//     ptr[1] = 3;
-//     ptr[2] = 5;
-//     ptr[3] = 7;
-//     ptr[4] = 9;
+int main()
+{
+    int *ptr;
+    ptr = (int *)calloc(5, sizeof(int));
+    ptr[0] = 1;
+    ptr[1] = 3;
+    ptr[2] = 5;
+    ptr[3] = 7;
+    ptr[4] = 9;
 
-//     printf("calloc\n");
-//     for (int i = 0; i < 5; i++)
-//     {
-//         printf(" %d \n", ptr[i]);
-//     }
+    printf("calloc\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf(" %d \n", ptr[i]);
+    }
 
-//     printf("realloc\n");
-//     ptr = realloc(ptr, 6);
-//     ptr[0] = 2;
-//     ptr[1] = 4;
-//     ptr[2] = 6;
-//     ptr[3] = 8;
-//     ptr[4] = 10;
-//     ptr[5] = 12;
+    printf("realloc\n");
+    ptr = realloc(ptr, 6);
+    ptr[0] = 2;
+    ptr[1] = 4;
+    ptr[2] = 6;
+    ptr[3] = 8;
+    ptr[4] = 10;
+    ptr[5] = 12;
 
-//     for (int i = 0; i < 6; i++)
-//     {
-//         printf("  %d \n", ptr[i]);
-//     }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("  %d \n", ptr[i]);
+    }
 
-// }
+}
 
 // // output
 // // calloc
@@ -49,62 +49,62 @@
 // // 10
 // // 12
 
-// #include <stdio.h>
-// int main()
-// {
-//     int car;
-//     int time;
-//     int signal;
-//     printf("enter a car speed range and time and single 0 = red  1= green ");
-//     scanf("%d %d %d", &car, &time, &signal);
-//     int state;
-//     if (signal == 1 && car <= 5)
-//     {
-//         state = 1;
-//     }
-//     else if (signal == 1 && car >= 5)
-//     {
-//         state = 2;
-//     }
-//     else if (signal == 0 && time < 10 && car <= 5)
-//     {
-//         state = 3;
-//     }
-//     else if (signal == 0 && time >= 10)
-//     {
-//         state = 4;
-//     }
-//     else if (signal == 0 && time < 10 && car >= 5)
-//     {
-//         state = 5;
-//     }else if(signal != 0 && signal != 1){
-//         state = 6;
-//     }
-//     switch (state)
-//     {
-//     case 1:
-//         printf("Go fast ");
-//         break;
-//     case 2:
-//         printf("Move slowly");
-//         break;
-//     case 3:
-//         printf("wati");
-//         break;
-//     case 4:
-//         printf("Ready ");
-//         break;
-//     case 5:
-//         printf("move fast sigal is red");
-//         break;
-//     case 6:
-//         printf("singal error ");
-//         break;
+#include <stdio.h>
+int main()
+{
+    int car;
+    int time;
+    int signal;
+    printf("enter a car speed range and time and single 0 = red  1= green ");
+    scanf("%d %d %d", &car, &time, &signal);
+    int state;
+    if (signal == 1 && car <= 5)
+    {
+        state = 1;
+    }
+    else if (signal == 1 && car >= 5)
+    {
+        state = 2;
+    }
+    else if (signal == 0 && time < 10 && car <= 5)
+    {
+        state = 3;
+    }
+    else if (signal == 0 && time >= 10)
+    {
+        state = 4;
+    }
+    else if (signal == 0 && time < 10 && car >= 5)
+    {
+        state = 5;
+    }else if(signal != 0 && signal != 1){
+        state = 6;
+    }
+    switch (state)
+    {
+    case 1:
+        printf("Go fast ");
+        break;
+    case 2:
+        printf("Move slowly");
+        break;
+    case 3:
+        printf("wati");
+        break;
+    case 4:
+        printf("Ready ");
+        break;
+    case 5:
+        printf("move fast sigal is red");
+        break;
+    case 6:
+        printf("singal error ");
+        break;
 
-//     default:
-//         break;
-//     }
-// }
+    default:
+        break;
+    }
+}
 
 // // // Output
 // // // enter a car speed range and time and single 0 = red  1= green 7 5 0
@@ -136,9 +136,38 @@ int maxno3(int a, int b, int c)
     {
          printf("c is gratest number %d",c);
     }
-    
+
 }
 
 // // Output
 // // Enter a three number 6 8 6
 ////  B is gratest number 8
+
+// Simple Searching Techinque Examples
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {10, 20, 30, 50, 70};
+    int key;
+    printf("Enter A Key to Find the Array Number : \n ");
+    scanf("%d", &key);
+    for (int i = 0; i <= 5; i++)
+    {
+        if (key == arr[i])
+        {
+            printf("You Enter Key Value  = %d and the find the Key = %d", arr[i], key);
+            return 0;
+        }
+    }
+    printf("Not Valid a number ");
+}
+
+// // OutPut
+// Enter A Key to Find the Array Number : 
+//  50
+// You Enter Key Value  = 50 and the find the Key = 50
+
+
+// //Enter A Key to Find the Array Number : 
+// // 60
+// //Not Valid a number 
