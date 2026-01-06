d87e413c886e6613b898ea486704aaecd477bb72
Shrishail <shrishailchincholi306@gmail.com>
2026-01-06 21:50:07 +0530

    DSA_exmples pratice swap 2 number

---
+++ DSA-program/swap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 20, b = 30;
+    printf("Before Swapping:\n");
+    printf("a:%d , b:%d\n", a, b);
+    swap(&a, &b);
+    printf("After Swapping:\n");
+    printf("a : %d , b : %d\n", a, b);
+    return 0;
+}
+// output
+// Before swapping:
+// a : 20, b: 30
+// After swapping:
+// a : 30, b : 20
