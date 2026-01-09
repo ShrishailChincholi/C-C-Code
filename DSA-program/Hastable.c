4804829fcaed2a54cb77a830659db4c9fc51c84a
Shrishail <shrishailchincholi306@gmail.com>
2026-01-09 21:14:39 +0530

    Dsa pratice

--- DSA-program/Hastable.c
+++ DSA-program/Hastable.c
@@ -10,3 +10,44 @@ struct Node {
     int value; 
     struct Node* next; 
 };
+// Structure for the hash table 
+struct HashTable { 
+    struct Node* table[SIZE]; 
+}; 
+ 
+// Function to initialize the hash table 
+void initializeHashTable(struct HashTable* ht) { 
+    for (int i = 0; i < SIZE; i++) { 
+        ht->table[i] = NULL; 
+    } 
+} 
+ 
+// Function to calculate the hash value for a key 
+int hash(char* key) { 
+    int sum = 0; 
+    for (int i = 0; key[i] != '\0'; i++) { 
+        sum += key[i]; 
+    } 
+    return sum % SIZE; 
+} 
+ 
+// Function to insert a key-value pair into the hash table 
+void insert(struct HashTable* ht, char* key, int value) { 
+    int index = hash(key); 
+ 
+    // Create a new node 
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node)); 
+    strcpy(newNode->key, key); 
+    newNode->value = value; 
+    newNode->next = NULL; 
+ 
+    // Insert the new node into the hash table
+        if (ht->table[index] == NULL) { 
+        ht->table[index] = newNode; 
+    } else { 
+        newNode->next = ht->table[index]; 
+        ht->table[index] = newNode; 
+    } 
+ 
+    printf("Key '%s' with value %d inserted into the hash table.\n", key, value); 
+} 
