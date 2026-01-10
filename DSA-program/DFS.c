dbcf2f46c746c73ef93846b2587e5f509bf06e7d
Shrishail <shrishailchincholi306@gmail.com>
2026-01-10 22:36:21 +0530

    Dsa pratice  DFC in C

--- DSA-program/DFS.c
+++ DSA-program/DFS.c
@@ -16,3 +16,24 @@ void dfs(int **adjMatrix, int *visited, int vertices, int currentVertex) {
         } 
     } 
 }
+int main() { 
+    int vertices, edges; 
+ 
+    // Get the number of vertices and edges from the user 
+    printf("Enter the number of vertices: "); 
+    scanf("%d", &vertices); 
+    printf("Enter the number of edges: "); 
+    scanf("%d", &edges); 
+ 
+    // Allocate memory for the adjacency matrix 
+    int **adjMatrix = (int **)malloc(MAX_VERTICES * sizeof(int *)); 
+    for (int i = 0; i < MAX_VERTICES; i++) { 
+        adjMatrix[i] = (int *)malloc(MAX_VERTICES * sizeof(int)); 
+    } 
+ 
+    // Initialize the adjacency matrix 
+    for (int i = 0; i < MAX_VERTICES; i++) { 
+        for (int j = 0; j < MAX_VERTICES; j++) { 
+            adjMatrix[i][j] = 0; 
+        } 
+    }
