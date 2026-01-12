d304973c039d600bd07817b33daebeba03f56e4a
Shrishail <shrishailchincholi306@gmail.com>
2026-01-12 14:23:42 +0530

    Dsa pratice-commited

---
+++ DSA-program/BTS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
