97ea73eae56323dfff677e70ec9846b91e8db61d
Shrishail <shrishailchincholi306@gmail.com>
2026-01-14 11:49:44 +0530

    DSA pratice

--- DSA-program/prefixtree.c
+++ DSA-program/prefixtree.c
@@ -16,3 +16,27 @@ struct TrieNode* createNode() {
         node->children[i] = NULL;
     return node;
 }
+
+void insert(struct TrieNode* root, char* key) {
+    struct TrieNode* temp = root;
+
+    for (int i = 0; key[i] != '\0'; i++) {
+        int index = key[i] - 'a';
+        if (!temp->children[index])
+            temp->children[index] = createNode();
+        temp = temp->children[index];
+    }
+    temp->isEndOfWord = 1;
+}
+
+int search(struct TrieNode* root, char* key) {
+    struct TrieNode* temp = root;
+
+    for (int i = 0; key[i] != '\0'; i++) {
+        int index = key[i] - 'a';
+        if (!temp->children[index])
+            return 0;
+        temp = temp->children[index];
+    }
+    return temp->isEndOfWord;
+}
