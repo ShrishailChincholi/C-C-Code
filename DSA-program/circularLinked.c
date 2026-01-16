f80f59126f0ed7c6d964767f42d4257f28ece438
Shrishail <shrishailchincholi306@gmail.com>
2026-01-16 21:16:21 +0530

    DSA Sloutions

--- DSA-program/circularLinked.c
+++ DSA-program/circularLinked.c
@@ -26,3 +26,30 @@ void insert(int x) {
     temp->next = newNode;
     newNode->next = head;
 }
+
+void display() {
+    struct Node* temp = head;
+    if (head == NULL) return;
+
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+
+    printf("(back to head)\n");
+}
+
+int main() {
+    insert(5);
+    insert(10);
+    insert(15);
+
+    printf("Circular Linked List:\n");
+    display();
+
+    return 0;
+}
+
+//output
+// Circular Linked List:
+// 5 -> 10 -> 15 -> (back to head)
