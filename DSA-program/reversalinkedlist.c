ad6ff9544f458443d9e88e78b06ab8848c6ca29d
Shrishail <shrishailchincholi306@gmail.com>
2026-02-03 22:45:10 +0530

    C pratice

---
+++ DSA-program/reversalinkedlist.c
@@ -0,0 +1,32 @@
+// Reverse a Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+struct Node* insertAtEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
