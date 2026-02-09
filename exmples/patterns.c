142dd046f260986dce132c4153646cc6224b4335
Shrishail <shrishailchincholi306@gmail.com>
2026-02-09 22:12:16 +0530

    C- pratice

--- exmples/patterns.c
+++ exmples/patterns.c
@@ -54,3 +54,27 @@ int main() {
 //  *****
 //   ***
 //    *
+
+
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++)
+            printf("  ");
+        for (int j = 1; j <= i; j++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
+
+
+// Output
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
