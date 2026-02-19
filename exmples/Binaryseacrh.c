3fcc784650c868c4fecdb06026454a89045d2eeb
Shrishail <shrishailchincholi306@gmail.com>
2026-02-19 22:49:58 +0530

    Binary search

--- exmples/Binaryseacrh.c
+++ exmples/Binaryseacrh.c
@@ -30,3 +30,41 @@ struct Node* insert(struct Node* root, int data){
     return root;
     
 }
+
+void inorder(struct Node* root){
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+    
+}
+
+void preorder(struct Node* root){
+    if (root != NULL)
+    {
+        printf("%d ",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+    
+}
+
+void postorder(struct Node* root){
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ",root->data);
+    }
+    
+}
+
+void menu(){
+    printf("\n--menu---\n");
+    printf("insert \n");
+    printf("inorder  \n");
+    printf("preorder \n");
+    printf("postorder \n");
+}
