d9b50f5ccfb07de9127e8ac2e1a2dc5b456eed37
Shrishail <shrishailchincholi306@gmail.com>
2026-02-22 22:00:15 +0530

    Practice

--- exmples/patterns.c
+++ exmples/patterns.c
@@ -156,3 +156,43 @@ int main() {
 //   *   *
 //  *     *
 // *********
+
+
+
+// Binary Search in C
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int key) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;         
+        else if (arr[mid] < key)
+            low = mid + 1;       
+        else
+            high = mid - 1;     
+    }
+    return -1;                  
+}
+
+int main() {
+    int arr[] = {5, 12, 18, 25, 33, 47, 59};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 25;
+
+    int result = binarySearch(arr, size, key);
+
+    if (result != -1)
+        printf("Element %d found at index %d\n", key, result);
+    else
+        printf("Element %d not found\n", key);
+
+    return 0;
+}
+
+// Output
+// Element 25 found at index 3
