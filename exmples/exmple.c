802b85df9e4d1f209b6ea6fff1dbd55d1fcb1f94
Shrishail <shrishailchincholi306@gmail.com>
2026-02-22 22:07:56 +0530

    stack example

--- exmples/exmple.c
+++ exmples/exmple.c
@@ -15,3 +15,39 @@ int main() {
 // Output
 
 // Little Endian
+
+// Stack operation
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int stack[MAX];
+int top = -1;
+
+
+int isFull() {
+    if (top == MAX - 1)
+        return 1;
+    else
+        return 0;
+}
+
+
+int isEmpty() {
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+
+void push(int value) {
+    if (isFull()) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+    } else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed into stack\n", value);
+    }
+}
