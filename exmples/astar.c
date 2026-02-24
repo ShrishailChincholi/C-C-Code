fe31db2faae780cf8584f643be12b02547ba2daa
Shrishail <shrishailchincholi306@gmail.com>
2026-02-24 21:44:14 +0530

    A* example

--- exmples/astar.c
+++ exmples/astar.c
@@ -43,3 +43,69 @@ char pop() {
 int idx(char c) {
     return c - 'A';
 }
+
+
+int main() {
+    // Graph
+    struct Edge graph[4][2] = {
+        {{'B',1}, {'C',4}},   // A
+        {{'D',3}},            // B
+        {{'D',1}},            // C
+        {}                    // D
+    };
+    int graphSize[4] = {2,1,1,0};
+
+    
+    int h[4] = {7,6,2,0};
+
+    int gCost[4];
+    char parent[4];
+    int visited[4] = {0};
+
+    for (int i = 0; i < 4; i++)
+        gCost[i] = INF;
+
+    char start = 'A', goal = 'D';
+
+    gCost[idx(start)] = 0;
+    push(start, h[idx(start)]);
+
+    while (pqSize > 0) {
+        char curr = pop();
+
+        if (curr == goal)
+            break;
+
+        visited[idx(curr)] = 1;
+
+        for (int i = 0; i < graphSize[idx(curr)]; i++) {
+            char neigh = graph[idx(curr)][i].to;
+            int w = graph[idx(curr)][i].weight;
+
+            int newCost = gCost[idx(curr)] + w;
+
+            if (newCost < gCost[idx(neigh)]) {
+                gCost[idx(neigh)] = newCost;
+                parent[idx(neigh)] = curr;
+                push(neigh, newCost + h[idx(neigh)]);
+            }
+        }
+    }
+
+    
+    printf("Path: ");
+    char path[10];
+    int p = 0;
+    char curr = goal;
+
+    while (curr != start) {
+        path[p++] = curr;
+        curr = parent[idx(curr)];
+    }
+    path[p++] = start;
+
+    for (int i = p - 1; i >= 0; i--)
+        printf("%c ", path[i]);
+
+    return 0;
+}
