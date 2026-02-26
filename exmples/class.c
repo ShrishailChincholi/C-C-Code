10cfcc904737ea11311fc7302d22cdf5eea59f52
Shrishail <shrishailchincholi306@gmail.com>
2026-02-26 23:06:53 +0530

    c examples

--- exmples/class.c
+++ exmples/class.c
@@ -1,29 +1,63 @@
 #include <iostream>
 using namespace std;
 
-class BankAccount {
+class BankAccount
+{
 private:
     int accNo;
     float balance;
 
 public:
-    
-    BankAccount(int a, float b) {
+
+    BankAccount(int a, float b)
+    {
         accNo = a;
         balance = b;
     }
 
-  
-    void deposit(float amount) {
+    void deposit(float amount)
+    {
         balance += amount;
         cout << "Amount Deposited: " << amount << endl;
     }
 
-    void withdraw(float amount) {
-        if (amount > balance) {
+    void withdraw(float amount)
+    {
+        if (amount > balance)
+        {
             cout << "Insufficient Balance!" << endl;
-        } else {
+        }
+        else
+        {
             balance -= amount;
             cout << "Amount Withdrawn: " << amount << endl;
         }
     }
+
+    void display()
+    {
+        cout << "Account No: " << accNo << endl;
+        cout << "Balance   : " << balance << endl;
+    }
+};
+
+int main()
+{
+    BankAccount acc1(1001, 5000); 
+
+    acc1.display();
+    acc1.deposit(2000);
+    acc1.withdraw(3000);
+    acc1.display();
+
+    return 0;
+}
+
+
+// Output:---
+// Account No: 1001
+// Balance   : 5000
+// Amount Deposited: 2000
+// Amount Withdrawn: 3000
+// Account No: 1001
+// Balance   : 4000
