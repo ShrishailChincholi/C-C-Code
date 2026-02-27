a45f3abde26a401fb1cc5902880d13c0e5906cb2
Shrishail <shrishailchincholi306@gmail.com>
2026-02-27 18:40:45 +0530

    c examples

--- exmples/linkedlist.c
+++ exmples/linkedlist.c
@@ -31,3 +31,40 @@ void insertEnd(int value) {
 
     temp->next = newNode;
 }
+
+void deleteNode(int value) {
+    struct Node *temp = head, *prev = NULL;
+
+    if (temp != NULL && temp->data == value) {
+        head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != value) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Value not found\n");
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void display() {
+    struct Node *temp = head;
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
