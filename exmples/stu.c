eba09bcabf9452cab5c907c4915fe930b9ebaf1a
Shrishail <shrishailchincholi306@gmail.com>
2026-03-07 23:11:29 +0530

    c examples

--- exmples/stu.c
+++ exmples/stu.c
@@ -128,3 +128,59 @@ int main()
 
     return 0;
 }
+
+
+
+
+// Matrix Multiplication in C
+
+#include <stdio.h>
+
+int main()
+{
+    int a[2][2], b[2][2], c[2][2];
+    int i, j, k;
+
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Matrix multiplication
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            c[i][j] = 0;
+            for(k = 0; k < 2; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("Result matrix:\n");
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
