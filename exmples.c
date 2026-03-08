e0449e4eca032635d716bce8511d05b134175207
Shrishail <shrishailchincholi306@gmail.com>
2026-03-08 23:39:23 +0530

    c examples

--- exmples.c
+++ exmples.c
@@ -410,3 +410,43 @@ int main()
 // Output
 // Normal Array:- 9  4  4  8  7  5  6 
 // Sorted Array:- 4  4  5  6  7  8  9 
+
+
+
+// Transpose of Matrix
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], i, j;
+
+    printf("Enter matrix elements:\n");
+
+    for(i=0;i<3;i++)
+        for(j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Transpose matrix:\n");
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//  
+// Enter matrix elements:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// Transpose matrix:
+// 1 4 7
+// 2 5 8
+// 3 6 9
