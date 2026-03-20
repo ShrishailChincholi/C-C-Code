70946766c3e0f00e90631b6bdaf2ae5a8150bf23
Shrishail <shrishailchincholi306@gmail.com>
2026-03-20 21:23:21 +0530

    c pratices

--- exmples/game1.c
+++ exmples/game1.c
@@ -68,3 +68,50 @@ int main() {
 
     return 0;
 }
+
+
+
+// Quize game
+#include <stdio.h>
+
+int main() {
+    int score = 0;
+    int answer;
+
+    printf("Simple Quiz Game\n");
+
+    // Question 1
+    printf("\n1. What is 2 + 2?\n");
+    printf("1) 3\n2) 4\n3) 5\n");
+    printf("Enter your answer: ");
+    scanf("%d", &answer);
+
+    if(answer == 2) {
+        score++;
+    }
+
+    // Question 2
+    printf("\n2. What is capital of India?\n");
+    printf("1) Mumbai\n2) Delhi\n3) Chennai\n");
+    printf("Enter your answer: ");
+    scanf("%d", &answer);
+
+    if(answer == 2) {
+        score++;
+    }
+
+    // Question 3
+    printf("\n3. Which language is used for C programming?\n");
+    printf("1) English\n2) Machine\n3) Programming Language\n");
+    printf("Enter your answer: ");
+    scanf("%d", &answer);
+
+    if(answer == 3) {
+        score++;
+    }
+
+    // Final Score
+    printf("\nYour score: %d / 3\n", score);
+
+    return 0;
+}
