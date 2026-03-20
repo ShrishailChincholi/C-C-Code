a0c0edcd4b02f766c06a7d10d0a9b38e8904a556
Shrishail <shrishailchincholi306@gmail.com>
2026-03-20 21:22:16 +0530

    c pratices

--- exmples/game1.c
+++ exmples/game1.c
@@ -31,3 +31,40 @@ int main() {
 
     return 0;
 }
+
+
+
+// Rock,  Paper, Scissors Game
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int user, computer;
+
+    // Random number for computer
+    srand(time(0));
+    computer = rand() % 3 + 1;
+
+    printf("Rock Paper Scissors Game\n");
+    printf("1. Rock\n2. Paper\n3. Scissors\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &user);
+
+    printf("Computer chose: %d\n", computer);
+
+    if(user == computer) {
+        printf("It's a draw!\n");
+    }
+    else if((user == 1 && computer == 3) ||
+            (user == 2 && computer == 1) ||
+            (user == 3 && computer == 2)) {
+        printf("You win!\n");
+    }
+    else {
+        printf("You lose!\n");
+    }
+
+    return 0;
+}
