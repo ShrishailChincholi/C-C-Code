f1d25574a84ffcd389cfb9616f0a893de617939b
Shrishail <shrishailchincholi306@gmail.com>
2026-03-22 22:06:24 +0530

    c pratices

--- exmples/partice.c
+++ exmples/partice.c
@@ -106,3 +106,29 @@ int main() {
     printf("Factorial = %d", fact);
     return 0;
 }
+
+
+//prime
+
+#include <stdio.h>
+int main() {
+    int n, i, flag = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(n == 1)
+        printf("Not Prime");
+    else if(flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
