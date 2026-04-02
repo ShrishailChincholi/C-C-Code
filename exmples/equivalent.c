6d6faba25ee9ce64212cf12ae38f110e69f11e06
Shrishail <shrishailchincholi306@gmail.com>
2026-04-02 21:52:52 +0530

    partice code

--- exmples/equivalent.c
+++ exmples/equivalent.c
@@ -35,3 +35,35 @@ int main() {
 
 // Output:
 // 2
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100], s2[100];
+    int i, j, c = 0;
+
+    scanf("%s %s", s1, s2);
+
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    int t = len1 + len2;
+
+    for (i = 0; i < len1; i++) {
+        for (j = 0; j < len2; j++) {
+            if (s1[i] == s2[j]) {
+                c += 2;
+                s2[j] = '*';
+                break;
+            }
+        }
+    }
+
+    printf("%d", t - c);
+
+    return 0;
+}
