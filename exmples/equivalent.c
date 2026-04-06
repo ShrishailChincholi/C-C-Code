881a61376810e969c23b95ff48dc59f633c256ad
Shrishail <shrishailchincholi306@gmail.com>
2026-04-06 22:50:11 +0530

    partice code

--- exmples/equivalent.c
+++ exmples/equivalent.c
@@ -175,3 +175,37 @@ int main() {
 
     return 0;
 }
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char string1[100], string2[100];
+    int i, j, c = 0;
+
+
+    scanf("%s", string1);
+    scanf("%s", string2);
+
+    int len1 = strlen(string1);
+    int len2 = strlen(string2);
+
+    int t = len1 + len2;
+
+    for (i = 0; i < len1; i++) {
+        for (j = 0; j < len2; j++) {
+            if (string1[i] == string2[j]) {
+                c += 2;
+                string2[j] = '*';  
+                break;
+            }
+        }
+    }
+
+    printf("%d", t - c);
+
+    return 0;
+}
