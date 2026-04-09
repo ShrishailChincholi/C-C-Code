d126eafe7777c1a5a570c781b0e28909a610827e
Shrishail <shrishailchincholi306@gmail.com>
2026-04-09 14:28:56 +0530

    partice code

--- exmples/equivalent.c
+++ exmples/equivalent.c
@@ -296,3 +296,28 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[100], res[100];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    for (int i = 0; i < n; i++) {
+        res[(i + k) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", res[i]);
+
+    return 0;
+}
