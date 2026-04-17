5f5cc7f1e6c1800d16a0c205a6c1a26f74a176d1
Shrishail <shrishailchincholi306@gmail.com>
2026-04-17 13:22:55 +0530

    C pratice

--- exmples/equivalent.c
+++ exmples/equivalent.c
@@ -467,5 +467,30 @@ int main() {
         return 0;
     }
 
+
+    
+    qsort(arr, n, sizeof(int), compare);
+
+    int longest = 1, current = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            continue; 
+        } else if (arr[i] == arr[i - 1] + 1) {
+            current++;
+        } else {
+            if (current > longest)
+                longest = current;
+            current = 1;
+        }
+    }
+
+    
+    if (current > longest)
+        longest = current;
+
+    printf("%d\n", longest);
+
+    return 0;
   
 }
