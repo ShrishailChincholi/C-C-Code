56e557eafd0968201531df98600f30e8a192c5fc
Shrishail <shrishailchincholi306@gmail.com>
2026-04-23 21:46:40 +0530

    C paratice

--- exmples/equivalent.c
+++ exmples/equivalent.c
@@ -624,3 +624,38 @@ int main() {
     printf("%d\n", count);
     return 0;
 }
+
+
+
+
+#include <stdio.h>
+
+int isValid(int arr[], int n) {
+    if (n <= 1) return 1;
+
+    int d = arr[1] - arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] <= arr[i-1] || arr[i] - arr[i-1] != d)
+            return 0;
+
+    return 1;
+}
+
+int main() {
+    int n, size, count = 0;
+    scanf("%d", &n);
+
+    while (n--) {
+        scanf("%d", &size);
+        int arr[size];
+
+        for (int i = 0; i < size; i++)
+            scanf("%d", &arr[i]);
+
+        if (isValid(arr, size))
+            count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
