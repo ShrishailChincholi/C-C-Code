f41ae7da28ac46954738ba87fb41f3b5e24614dc
Shrishail <shrishailchincholi306@gmail.com>
2026-05-13 19:56:15 +0530

    Pratice c code

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -434,3 +434,21 @@ void gray(int n) {
 int main() {
     gray(3);
 }
+
+
+// Ackermann Function
+#include <stdio.h>
+
+int ack(int m, int n) {
+    if (m == 0)
+        return n + 1;
+
+    if (n == 0)
+        return ack(m - 1, 1);
+
+    return ack(m - 1, ack(m, n - 1));
+}
+
+int main() {
+    printf("%d", ack(2,2));
+}
