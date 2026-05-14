5b234d53845f8c75753e37e12102985cec148a22
Shrishail <shrishailchincholi306@gmail.com>
2026-05-14 21:50:18 +0530

    Pratice c code

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -530,3 +530,24 @@ int win(int n) {
 int main() {
     printf(win(5) ? "Win" : "Lose");
 }
+
+
+// Sieve of Eratosthenes
+
+#include <stdio.h>
+
+int main() {
+    int n = 50, prime[51];
+
+    for (int i = 2; i <= n; i++)
+        prime[i] = 1;
+
+    for (int i = 2; i * i <= n; i++)
+        if (prime[i])
+            for (int j = i * i; j <= n; j += i)
+                prime[j] = 0;
+
+    for (int i = 2; i <= n; i++)
+        if (prime[i])
+            printf("%d ", i);
+}
