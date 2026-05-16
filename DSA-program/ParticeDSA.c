d39129897e072fde531cc90aebb9ad570e46ba6c
Shrishail <shrishailchincholi306@gmail.com>
2026-05-16 22:44:42 +0530

    Practice c code

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -566,3 +566,23 @@ int sum(int n) {
 int main() {
     printf("%d", sum(9876));
 }
+
+
+// Matrix Diagonal Sum Trick
+
+#include <stdio.h>
+
+int main() {
+    int a[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++)
+        sum += a[i][i] + a[i][2-i];
+
+    printf("%d", sum - a[1][1]);
+}
