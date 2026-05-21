66658c071c13bf14e10354d7c6425216e982a5b6
Shrishail <shrishailchincholi306@gmail.com>
2026-05-21 21:42:26 +0530

    Practice c code

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -704,3 +704,14 @@ int duplicate(int a[]) {
 
     return slow;
 }
+
+
+
+#include <stdio.h>
+
+int xorN(int n) {
+    if (n % 4 == 0) return n;
+    if (n % 4 == 1) return 1;
+    if (n % 4 == 2) return n + 1;
+    return 0;
+}
