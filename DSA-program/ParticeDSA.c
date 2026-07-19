88929f6c46036b41e587b1c09488eecf5a0b7637
Shrishail <shrishailchincholi306@gmail.com>
2026-07-19 22:10:04 +0530

    Partice c code

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -1500,3 +1500,26 @@ int main()
     return 0;
 }
 
+
+
+/////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void sort(int a[], int n)
+{
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(a[i]>a[j])
+                swap(&a[i],&a[j]);
+}
+
