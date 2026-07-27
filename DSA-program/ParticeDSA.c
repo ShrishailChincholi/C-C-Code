3586ee1b714d07f77468bc237d14fcf19d351275
Shrishail <shrishailchincholi306@gmail.com>
2026-07-27 23:07:08 +0530

    c program partice

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -1830,3 +1830,38 @@ void build(int node, int start, int end, int arr[]) {
 
     tree[node] = tree[2 * node] + tree[2 * node + 1];
 }
+
+void update(int node, int start, int end, int l, int r, int val) {
+
+    if (lazy[node] != 0) {
+        tree[node] += (end - start + 1) * lazy[node];
+
+        if (start != end) {
+            lazy[2 * node] += lazy[node];
+            lazy[2 * node + 1] += lazy[node];
+        }
+
+        lazy[node] = 0;
+    }
+
+    if (start > r || end < l)
+        return;
+
+    if (start >= l && end <= r) {
+        tree[node] += (end - start + 1) * val;
+
+        if (start != end) {
+            lazy[2 * node] += val;
+            lazy[2 * node + 1] += val;
+        }
+
+        return;
+    }
+
+    int mid = (start + end) / 2;
+
+    update(2 * node, start, mid, l, r, val);
+    update(2 * node + 1, mid + 1, end, l, r, val);
+
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
+}
