2bdfc3c95e8c6eb52aadbc529fbb42d71856d3c3
Shrishail <shrishailchincholi306@gmail.com>
2026-07-30 19:15:52 +0530

    c program partice letcode

---
+++ DSA-program/letcode.c
@@ -0,0 +1,54 @@
+//  LeetCode 139 – Word Break
+
+#include <stdio.h>
+#include <string.h>
+
+int search(char *s, int start, int end, char *dict[], int size)
+{
+    char temp[100];
+    int len = end - start;
+
+    strncpy(temp, s + start, len);
+    temp[len] = '\0';
+
+    for(int i = 0; i < size; i++)
+        if(strcmp(temp, dict[i]) == 0)
+            return 1;
+
+    return 0;
+}
+
+int wordBreak(char *s, char *dict[], int size)
+{
+    int n = strlen(s);
+    int dp[101] = {0};
+
+    dp[0] = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(dp[j] && search(s, j, i, dict, size))
+            {
+                dp[i] = 1;
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+}
+
+int main()
+{
+    char str[] = "leetcode";
+    char *dict[] = {"leet", "code"};
+
+    if(wordBreak(str, dict, 2))
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
