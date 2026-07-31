0c352f667d84aff0ef3057f5df063b5d09616cb9
Shrishail <shrishailchincholi306@gmail.com>
2026-07-31 22:04:29 +0530

    c program partice letcode 128

--- DSA-program/letcode.c
+++ DSA-program/letcode.c
@@ -52,3 +52,20 @@ int main()
 
     return 0;
 }
+
+
+// LeetCode 128 – Longest Consecutive Sequence
+
+#include <stdio.h>
+
+void sort(int a[], int n)
+{
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(a[i]>a[j])
+            {
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+}
