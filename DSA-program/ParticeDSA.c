5088e828bdb8d204c0da769c0a25ff4eb57a670b
Shrishail <shrishailchincholi306@gmail.com>
2026-09-12 21:00:08 +0530

    LeetCode 322

--- DSA-program/ParticeDSA.c
+++ DSA-program/ParticeDSA.c
@@ -2133,3 +2133,34 @@ int main() {
 
     return 0;
 }
+
+
+// LeetCode 322 — Coin Change
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int coinChange(int* coins, int n, int amount) {
+    int dp[amount + 1];
+
+    for (int i = 1; i <= amount; i++)
+        dp[i] = amount + 1;
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++)
+        for (int j = 0; j < n; j++)
+            if (coins[j] <= i)
+                dp[i] = dp[i] < dp[i - coins[j]] + 1
+                      ? dp[i] : dp[i - coins[j]] + 1;
+
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int main() {
+    int coins[] = {1, 2, 5};
+
+    printf("%d", coinChange(coins, 3, 11));
+
+    return 0;
+}
